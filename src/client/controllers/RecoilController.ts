8f3c8cfbd3d576190e052b6e2d131adf17c43d7b
AlphaRunic <roleyspersonal@gmail.com>
2022-11-06 16:09:20 -0800

    Make recoil torque faster

--- src/client/controllers/RecoilController.ts
+++ src/client/controllers/RecoilController.ts
@@ -5,13 +5,13 @@ import Spring from "shared/modules/utility/Spring";
 export class RecoilController implements OnRender {
 	private readonly attached: (Camera | BasePart)[] = [];
     private readonly mainSpringDefaults = [25, 75, 4, 5.5];
-    private readonly torqueSpringDefaults = [10, 110, 4, 8];
+    private readonly torqueSpringDefaults = [50, 110, 4, 15];
     private readonly mainSpring = new Spring(...this.mainSpringDefaults);
     private readonly torqueSpring = new Spring(...this.torqueSpringDefaults);
     private readonly modifiers = {
         recoverSpeed: 1,
         kickSpeed: 1,
-        kickMultiplier: 1.15,
+        kickMultiplier: 1,
         kickDamper: 1,
     };
 
@@ -30,14 +30,14 @@ export class RecoilController implements OnRender {
     kick(force: Vector3): void {
         const [mainDefaultMass, mainDefaultForce, mainDefaultDamper, mainDefaultSpeed] = this.mainSpringDefaults;
         this.mainSpring.mass = mainDefaultMass / this.modifiers.recoverSpeed;
-        this.mainSpring.force = mainDefaultForce * this.modifiers.kickMultiplier;
+        this.mainSpring.force = mainDefaultForce / this.modifiers.kickMultiplier;
         this.mainSpring.damping = mainDefaultDamper * this.modifiers.kickDamper;
         this.mainSpring.speed = mainDefaultSpeed * this.modifiers.kickSpeed;
         this.mainSpring.shove(force);
 
         const [torqueDefaultMass, torqueDefaultForce, torqueDefaultDamper, torqueDefaultSpeed] = this.torqueSpringDefaults;
         this.torqueSpring.mass = torqueDefaultMass / this.modifiers.recoverSpeed;
-        this.torqueSpring.force = torqueDefaultForce * this.modifiers.kickMultiplier;
+        this.torqueSpring.force = torqueDefaultForce / this.modifiers.kickMultiplier;
         this.torqueSpring.damping = torqueDefaultDamper * this.modifiers.kickDamper;
         this.torqueSpring.speed = torqueDefaultSpeed * this.modifiers.kickSpeed;
         this.torqueSpring.shove(force);
